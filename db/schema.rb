6796b97f4e1dc4f811e26e09293a34ccd0125275
rajendki <Kishore.rajendran@freshworks.com>
2021-05-20 12:12:17 +0530

    meetings-tab-changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_051603) do
+ActiveRecord::Schema.define(version: 2021_05_19_174438) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,11 @@ ActiveRecord::Schema.define(version: 2021_05_19_051603) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_id"
+    t.string "card_brand"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.string "card_last_4"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
